d6137cd638fe0c4259487a7f8dc36a62471c3d66
Duzhinsky <w1vvv1w@ya.ru>
2024-08-05 10:27:17 +0300

    Fix missing field in copy constructor

--- temporal-sdk/src/main/java/io/temporal/worker/WorkerOptions.java
+++ temporal-sdk/src/main/java/io/temporal/worker/WorkerOptions.java
@@ -113,6 +113,7 @@ public final class WorkerOptions {
       this.useBuildIdForVersioning = o.useBuildIdForVersioning;
       this.buildId = o.buildId;
       this.stickyTaskQueueDrainTimeout = o.stickyTaskQueueDrainTimeout;
+      this.identity = o.identity;
     }
 
     /**
